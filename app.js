3d2336bb246f396519c6e9448101d2b6245717be
Mayank Tripathi <tripathimayank216@gmail.com>
2023-03-09 19:08:42 +0530

    Update file changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb+srv://admin-mayank:%40NYDEVA%4025@cluster0.wlsli6q.mongodb.net/todolistDB");
+mongoose.connect(DATABASE_URL);
 
 const itemsSchema = {
   name: String
